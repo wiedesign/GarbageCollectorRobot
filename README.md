50326161b237bc68c5cc50c0d7a67110ed4c4347
Izyl Canonicato <izylc.grin@gmail.com>
2020-11-21 22:16:47 -0800

    Basic routing of pages and sets the landing page to be the about page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GarbageCollectorRobot
+
+oh yenno...
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className='app'>
+    <h1>WiE Garbage Collector Robot</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact activeClassName="current" to='/'>About Us</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/robotvalidation'>Robot Control</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div className='about'>
+    <h1>WiE Design welcomes you to...</h1>
+    <p> Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?.</p>
+  </div>
+);
+
+const GetStarted = () => (
+  <div className='robotvalidation'>
+    <h1>Get Started</h1>
+    <p>In order to use the WiE robot, you will need a couple things:</p>
+  </div>
+);
+
+const Main = () => (
+  <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/robotvalidation' component={GetStarted}></Route>
+  </Switch>
+);
 
 export default App;
