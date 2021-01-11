5845b6bb240dfe61c1abe553ad3ab91f0dbbdb7f
sarah raisuddin <sarah58091@hotmail.ca>
2021-01-10 20:38:15 -0800

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,17 @@ import './Footer.css';
 
 export default function Footer() {
     return (
+        
         <div className="footer">
-            <p>Hello</p>
+            
+        
+            <ul id="flex-container">
+                <li class="flex-item">Contact Us</li>
+                <li class="flex-item">instagram:<a href="#">@wiesfu</a></li>
+                <li class="flex-item">facebook:<a href="#">SFU WiE (Women in Engineering)</a></li>
+                <li class="flex-item">email: wie@sfu.ca</li>
+            </ul>
+            
         </div>
     )
 }
