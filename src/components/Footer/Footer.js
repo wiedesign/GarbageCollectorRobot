1819e3e9291d585afba3b81884a8f9610f3fd95f
sarah raisuddin <sarah58091@hotmail.ca>
2021-01-13 17:03:50 -0800

    submarine update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './Footer.css';
+import fb from './facebook.png';
+import ig from './instagram.png';
+import linkedin from './linkedin.png';
 
 export default function Footer() {
     return (
         
         <div className="footer">
             
-        
+
+            
+
             <ul id="flex-container">
                 <li id="title" class="flex-item">Contact Us</li>
-                <li class="flex-item">instagram:<a href="#">@wiesfu</a></li>
-                <li class="flex-item">facebook:<a href="#">SFU WiE (Women in Engineering)</a></li>
                 <li class="flex-item">email: wie@sfu.ca</li>
+                <div className="icons">
+                <li class="flex-item" ><a href="#"><img src={fb}></img></a></li>
+                <li class="flex-item" ><a href="#"><img src={linkedin}></img></a></li>
+                <li class="flex-item" ><a href="#"><img src={ig}></img></a></li>
+                </div>
+
+                
+                
             </ul>
             
         </div>
