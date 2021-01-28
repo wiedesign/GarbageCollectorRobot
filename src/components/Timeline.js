835076eed9dfe5a6b426040525b89d2d5c3e17b7
Dexter Bigueta <dbigueta@sfu.ca>
2021-01-27 18:30:04 -0800

    Added Project Progress Section

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Timeline() {
-
-    const [activeClass, setActiveClass] = useState('');   
+ 
 
     return (
         <div className ="timeline">
@@ -14,23 +13,35 @@ export default function Timeline() {
             <div className="timeline-submarine">
                 <p className="timeline-submarine-title">Project Stages</p>
                 <div className="timeline-submarine-windows">
-                    <p onClick={() => setActiveClass('timeline-active1')}>1</p>
-                    <p onClick={() => setActiveClass('timeline-active2')}>2</p>
-                    <p onClick={() => setActiveClass('timeline-active3')}>3</p>
-                    <p onClick={() => setActiveClass('timeline-active4')}>4</p>
-                    <p onClick={() => setActiveClass('timeline-active5')}>5</p>
-                    <p onClick={() => setActiveClass('timeline-active6')}>6</p>
-                    <p onClick={() => setActiveClass('timeline-active7')}>7</p>
+                    <div>
+                        <p>1</p>
+                        <p>Research</p>
+                    </div>
+                    <div>
+                        <p>2</p>
+                        <p>Design</p>
+                    </div>
+                    <div>
+                        <p>3</p>
+                        <p>Prototype</p>
+                    </div>
+                    <div>
+                        <p>4</p>
+                        <p>Testing</p>
+                    </div>
+                    <div>
+                        <p>5</p>
+                        <p>Design<br />Improvements</p>
+                    </div>
+                    <div>
+                        <p>6</p>
+                        <p>Production</p>
+                    </div>
+                    <div>
+                        <p>7</p>
+                        <p>Final<br />Testing</p>
+                    </div>
                 </div>
-            </div>
-            <div className="timeline-stage">
-                <p>Lorem Lorem ex veniam nostrud tempor eu aliqua exercitation reprehenderit sint aute pariatur consectetur.</p>
-                <p>Commodo commodo dolor sint ipsum.</p>
-                <p>Magna irure reprehenderit incididunt ex sit sit veniam voluptate amet excepteur amet consequat.</p>
-                <p>Mollit enim fugiat labore duis et sunt sint.</p>
-                <p>Sit est veniam duis non deserunt excepteur qui.</p>
-                <p>Reprehenderit do ipsum duis occaecat irure ad nisi commodo est.</p>
-                <p>Cupidatat nulla amet amet in velit cillum elit exercitation cillum.</p>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,11 @@ import React from 'react';
 import FirstPage from '../components/FirstPage';
 import AboutIsland from '../components/AboutIsland';
 import MeetOurRobot from '../components/MeetOurRobot';
+import ModelView from '../components/ModelView';
 import NameIsland from '../components/NameIsland';
 import Timeline from '../components/Timeline';
+import ProjectProgress from '../components/ProjectProgress';
 import HomeControls from '../components/HomeControls';
-import ModelView from '../components/ModelView';
 
 export default function Home() {
     return (
@@ -17,6 +18,7 @@ export default function Home() {
             <ModelView />
             <NameIsland />
             <Timeline />
+            <ProjectProgress />
             <HomeControls />
         </div>  
     )
