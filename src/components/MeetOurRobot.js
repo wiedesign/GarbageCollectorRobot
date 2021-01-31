e2df8f0a37b1a91dc4253e19573a1ae38bbbea24
Dexter Bigueta <dexter@course.studio>
2021-01-30 19:11:47 -0800

    Updating website

--- src/components/MeetOurRobot.js
+++ src/components/MeetOurRobot.js
@@ -5,15 +5,15 @@ export default function MeetOurRobot() {
     <div className="meet-robot">
         <img className="meet-robot-turtle" src="/images/turtle2.png" alt="" />
             <div className="meet-robot-top">
-                <div className="meet-robot-bubble1" />
-                <div className="meet-robot-bubble2" />
-                <div className="meet-robot-bubble3" />
+                <div className="meet-robot-bubble bubble-1" />
+                <div className="meet-robot-bubble bubble-2" />
+                <div className="meet-robot-bubble bubble-3" />
             </div>
             <p>Meet<br/>Our<br/>Robot</p>
             <div className="meet-robot-bottom">
-                <div className="meet-robot-bubble1" />
-                <div className="meet-robot-bubble2" />
-                <div className="meet-robot-bubble3" />
+                <div className="meet-robot-bubble bubble-4" />
+                <div className="meet-robot-bubble bubble-5" />
+                <div className="meet-robot-bubble bubble-6" />
             </div>
             <div className="model-view">
                 <model-viewer
