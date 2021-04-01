c989d1e009fa8983fd427161846800c607f41e53
Dexter Bigueta <dexter@course.studio>
2021-03-31 19:42:15 -0700

    Refactoring Code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
+import Controls from './pages/Controls';
 import Home from './pages/Home';
 
 import Footer from './components/Footer';
@@ -14,6 +15,7 @@ export default class App extends Component {
             <Router>
                 <Navbar />
                 <Switch>
+                    <Route path="/controls" exact component={Controls} />
                     <Route path="/" exact component={Home} />
                 </Switch>
                 <Footer />
