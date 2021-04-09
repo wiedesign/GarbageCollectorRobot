5a2a5a1c56a5bdf329e59d346fb7c737c7fd48bb
Dexter Bigueta <dexter@course.studio>
2021-04-08 18:01:42 -0700

    Added button to switch from home and controls pages
    Ref #12
    
    Done ")

--- src/pages/Controls.js
+++ src/pages/Controls.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Controls() {
     return (
         <section className="controls">
-            
+            <Link to="/"><img src="/images/logo.png" alt="" /></Link>
         </section>
     )
 }
