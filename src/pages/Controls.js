4fdd59954fef1e2697f02f200ca413bdfa6b611f
sarah raisuddin <sarah58091@hotmail.ca>
2021-05-03 21:45:41 -0700

    Sarah/9/update-website-layout
    Ref #9
    
    - added controllers and buttons
    - dimensions are hardcoded atm
    - still need to tweak arrows on button,
      might have to use images for all of them

--- src/pages/Controls.js
+++ src/pages/Controls.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import ActionControls from '../components/ActionControls'
+import MovementControls from '../components/MovementControls'
 
 export default function Controls() {
     return (
         <section className="controls">
             <Link to="/"><img src="/images/logo.png" alt="" /></Link>
+                <div className="controls__connectionIndicators">
+                    <button className="controls__connectionIndicators__ble"/>
+                    <button className="controls__connectionIndicators__wifi"/>
+                </div>
+                <button className="controls__clawOpenbtn">Claw Open</button>
+                <div className="controls__bottomSection">
+                    <div className="controls__bottomSection__actionControls">
+                        <ActionControls />
+                    </div>
+                    <img className="controls__bottomSection__blue-robot-trash" src="/images/controller/blue-robot-trash.svg" alt="" />
+                    <div className="controls__bottomSection__movementControls">
+                        <MovementControls />
+                    </div>
+                </div>
         </section>
     )
 }
