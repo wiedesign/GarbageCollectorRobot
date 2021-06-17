01a6753320d488b0bd0c5d0a5a03fb9aca04fece
Rose <rosie.epstein@shaw.ca>
2021-06-16 23:00:05 -0700

    Practice HTML/CSS Login Submission Form
    Ref #23
    
    Associated CSS/HTML files for practice login submission form.

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -3,9 +3,28 @@ import React from 'react'
 const Demo: React.FC = () =>{
     return (
         <section className="demo">
-            <p id="text" className="text">Hello</p>
+            <div className="flexbox-container-1">
+                <div className="flexbox-item">
+                        <p className="login-form header">Login Form</p>
+                        <p className="login-form other-text">Login to access your dashboard</p>
+                        <form>
+                            <div>
+                                <input type="text" className="inputs email" name="email" placeholder="  email@example.ca"></input>
+                            </div>
+                            <div>    
+                                <input type="password" className="inputs pwd" name="password" placeholder="  password"></input>
+                            </div>
+                            <div>
+                            <input type="submit" className="btn" value="LOGIN"></input>
+                            </div>
+                        </form>
+                        <p className="login-form other-text">Don't have an account? 
+                            <a href="https://wiesfu-garbage-collector-robot.web.app/">Sign Up</a>
+                        </p>
+                </div>
+            </div>
         </section>
     )
-};
+}
  
 export default Demo;
